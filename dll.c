6170bfa23e29337bd740595bb31a05534c9c2432
yue-studio <47492421+yue-studio@users.noreply.github.com>
2022-03-01 14:55:09 -0800

    Create dll.c

---
+++ dll.c
@@ -0,0 +1,110 @@
+/*
+ * Example code from:
+ * https://www.geeksforgeeks.org/doubly-linked-list/
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Node of a doubly linked list */
+struct Node {
+    int data;
+    struct Node* next; 
+    struct Node* prev;
+};
+
+/* Given a reference (pointer to pointer) to the head of a list
+   and an int, inserts a new node on the front of the list. */
+void push(struct Node** head_ref, int new_data)
+{
+    /* 1. allocate node */
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+ 
+    /* 2. put in the data  */
+    new_node->data = new_data;
+ 
+    /* 3. Make next of new node as head and previous as NULL */
+    new_node->next = (*head_ref);
+    new_node->prev = NULL;
+ 
+    /* 4. change prev of head node to new node */
+    if ((*head_ref) != NULL)
+        (*head_ref)->prev = new_node;
+ 
+    /* 5. move the head to point to the new node */
+    (*head_ref) = new_node;
+}
+
+/* Given a reference (pointer to pointer) to the head
+   of a DLL and an int, appends a new node at the end  */
+void append(struct Node** head_ref, int new_data)
+{
+    /* 1. allocate node */
+    struct Node* new_node
+        = (struct Node*)malloc(sizeof(struct Node));
+ 
+    struct Node* last = *head_ref; /* used in step 5*/
+ 
+    /* 2. put in the data  */
+    new_node->data = new_data;
+ 
+    /* 3. This new node is going to be the last node, so
+          make next of it as NULL*/
+    new_node->next = NULL;
+ 
+    /* 4. If the Linked List is empty, then make the new
+          node as head */
+    if (*head_ref == NULL) {
+        new_node->prev = NULL;
+        *head_ref = new_node;
+        return;
+    }
+ 
+    /* 5. Else traverse till the last node */
+    while (last->next != NULL)
+        last = last->next;
+ 
+    /* 6. Change the next of last node */
+    last->next = new_node;
+ 
+    /* 7. Make last node as previous of new node */
+    new_node->prev = last;
+ 
+    return;
+}
+
+// This function prints contents of linked list starting
+// from the given node
+void printList(struct Node* node)
+{
+    struct Node* last;
+    printf("\nTraversal in forward direction \n");
+    while (node != NULL) {
+        printf(" %d ", node->data);
+        last = node;
+        node = node->next;
+    }
+ 
+    printf("\nTraversal in reverse direction \n");
+    while (last != NULL) {
+        printf(" %d ", last->data);
+        last = last->prev;
+    }
+}
+
+int main()
+{
+    /* Start with the empty list */
+    struct Node* head = NULL;
+ 
+    push(&head, 7);
+ 
+    push(&head, 1);
+    
+    append(&head, 6);
+    
+    printf("Created DLL is: ");
+    printList(head);
+
+    return 0;
+}
